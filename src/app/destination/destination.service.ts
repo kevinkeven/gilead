b983899c85c62abf41d5c0df1ffac7ce84d0ed94
kevinkeven20 <kevinkeven20@gmail.com>
2023-05-27 15:51:51 +0300

    changed destination api

--- src/app/destination/destination.service.ts
+++ src/app/destination/destination.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SharedService } from '../shared/shared.service';
+import { environment } from 'src/environments/environment';
+environment
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class DestinationService {
-  APIURL = 'http://127.0.0.1:8000/api/';
+  APIURL!: string;
   destinationApiUrl = `${this.APIURL}destination/`;
   countryApiUrl = `${this.APIURL}shared/country/`;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.APIURL = environment.APIURL;
+  }
 
   getDestinations() {
     return this.http.get(this.destinationApiUrl + 'list/');
