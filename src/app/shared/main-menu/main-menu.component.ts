2ed9a2619cd7794c4d378dcaae8d327a80ae67a0
kevinkeven20 <kevinkeven20@gmail.com>
2023-06-28 17:10:36 +0300

    changed route methods

--- src/app/shared/main-menu/main-menu.component.ts
+++ src/app/shared/main-menu/main-menu.component.ts
@@ -1,19 +1,25 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { SharedService } from '../shared.service';
 import { DestinationService } from 'src/app/destination/destination.service';
-DestinationService
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.css']
+  styleUrls: ['./main-menu.component.css'],
 })
 export class MainMenuComponent {
   ToggleMegamenu = false;
   countries: any;
   destinations: any;
 
-  constructor(private sharedService: SharedService, private destinationService: DestinationService) { }
+  constructor(
+    private sharedService: SharedService,
+    private destinationService: DestinationService,
+    private router: Router,
+    private location: Location
+  ) {}
 
   ngOnInit() {
     this.sharedService.Countries().subscribe({
@@ -23,11 +29,25 @@ export class MainMenuComponent {
       error: (error) => {
         console.log(error);
       },
-    });this.destinationService.getDestinations().subscribe({
+    });
+    this.destinationService.getDestinations().subscribe({
       next: (data) => {
         this.destinations = data;
-      }
-    })
+      },
+    });
   }
-
+  todestination(slug: any) {
+    this.router
+      .navigate(['/destination', slug], { skipLocationChange: true })
+      .then(() => {
+        this.location.replaceState(`/destination/${slug}`);
+        window.location.reload();
+      });
+  }
+  toCountry(slug: any) {
+    this.router.navigate(['/', slug], { skipLocationChange: true }).then(() => {
+      this.location.replaceState(`/${slug}`);
+      window.location.reload();
+    });
+  }
 }
